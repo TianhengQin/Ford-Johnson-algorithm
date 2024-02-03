d7f54c2545c63ff33ee49c9818da9c23d92f1363
TianhengQin <134354910+TianhengQin@users.noreply.github.com>
2024-02-03 05:45:56 +0100

    Update FordJohnsonAlgorithm.cpp
    update insert order

--- FordJohnsonAlgorithm.cpp
+++ FordJohnsonAlgorithm.cpp
@@ -4,13 +4,38 @@
 # include <sstream>
 # include <cstdlib>
 
+std::vector<int> gOrder(int k) {
+    std::vector<int> re;
+
+    int prev = 2;
+    int curr;
+    int i = 1;
+    int g = 2;
+    int size = 2;
+
+    re.push_back(2);
+    re.push_back(1);
+
+    while (++i < k) {
+        size *=2;
+        g = size - g;
+        curr = prev + g;
+        while (curr > prev)
+            re.push_back(curr--);
+        prev = prev + g;
+    }
+    return re;
+}
+
+std::vector<int> order = gOrder(13);
+
 void printV(std::vector<int> &lst) {
     std::vector<int>::iterator itl;
     int i = 0;
     for (itl = lst.begin(); itl != lst.end(); ++itl) {
         i++;
         std::cout << *itl << " ";
-        if (i == 20)
+        if (i == 10)
             break;
     }
     if (itl != lst.end() && ++itl != lst.end())
@@ -18,11 +43,18 @@ void printV(std::vector<int> &lst) {
     std::cout << std::endl;
 }
 
-void insertSizeV(std::vector<int> &a, std::vector<int>::iterator b, std::vector<int>::iterator c, int size) {
+void swapSizeV(std::vector<int> &a, std::vector<int>::iterator b, std::vector<int>::iterator c, int size) {
 
     std::vector<int>::iterator d = c + size;
     std::vector<int> ins(c, d);
     a.erase(c, d);
+    a.insert(b, ins.begin(), ins.end());
+}
+
+void insertSizeV(std::vector<int> &a, std::vector<int>::iterator b, std::vector<int>::iterator c, int size) {
+
+    std::vector<int>::iterator d = c + size;
+    std::vector<int> ins(c, d);
     a.insert(b, ins.begin(), ins.end());
 }
 
@@ -57,41 +89,60 @@ void sortV(std::vector<int> &a) {
     int tab;
     std::vector<int>::iterator j;
     std::vector<int>::iterator k;
+    std::vector<int> sort;
+
     while (2 * size <= len) {
         
         tab = len / (2 * size);
         j = a.begin();
         k = j + size;
         if (*j < *k) {
-            insertSizeV(a, j, k, size);
+            swapSizeV(a, j, k, size);
         }
         for (int i = 1; i < tab; i++) {
             j += 2 * size;
             k += 2 * size;
             if (*j < *k) {
-                insertSizeV(a, j, k, size);
+                swapSizeV(a, j, k, size);
             }
         }
         size *= 2;
     }
     size /= 2;
-
+    int sortSize;
+    int o;
     while (size >= 1) {
+        sort.clear();
         j = a.begin();
         k = j + size;
-        insertSizeV(a, j, k, size);
+        while (distance(j, a.end()) >= 2 * size) {
+            insertSizeV(sort, sort.end(), j, size);
+            j += 2 * size;
+        }
+        insertSizeV(sort, sort.begin(), k, size);
         tab = len / (size * 2);
+        o = 0;
+        sortSize = 2;
         for (int i = 1; i < tab; i++) {
-            k += 2 * size;
-            j = binSearchV(a, size, k, *k);
-            insertSizeV(a, j, k, size);
+            if (o == 0 || order[o] > order[o - 1])
+                sortSize *= 2;
+            if (sortSize > sort.size() / size)
+                sortSize = sort.size() / size;
+            while (order[o] >= tab)
+                o++;
+            k = a.begin() + size + 2 * size * order[o++];
+            j = binSearchV(sort, size, sort.begin() + (sortSize - 1) * size, *k);
+            insertSizeV(sort, j, k, size);
         }
         if ((len / size) % 2) {
-            k += size;
-            j = binSearchV(a, size, k, *k);
-            insertSizeV(a, j, k, size);
+            k = a.begin() + size * 2 * tab;
+            j = binSearchV(sort, size, sort.end(), *k);
+            insertSizeV(sort, j, k, size);
         }
         size /= 2;
+        j = a.end() - (a.size() - sort.size());
+        sort.insert(sort.end(), j, a.end());
+        a = sort;
     }
 }
 
@@ -118,6 +169,8 @@ int main(int argi, char **argv) {
         i = std::atoi(s.c_str());
         vct.push_back(i);
     }
+    std::cout << "Order: ";
+    printV(order);
     std::cout << "Before: ";
     printV(vct);
     time_t start, end;
